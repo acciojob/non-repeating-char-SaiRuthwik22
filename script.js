633c1b538040536a1ce3a84eb7edbde2ceaa0ea9
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 13:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	      let frequencyMap = {}
+ for(let i=0;i<str.length;i++){
+     let char = str[i]
+     if(frequencyMap[char]){
+         frequencyMap[char]+=1
+     }
+     else{
+         frequencyMap[char] = 1
+     }
+ }
+ for(key in frequencyMap){
+     if(frequencyMap[key]==1){
+         return key
+     }
+ }
+ return "null"
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
